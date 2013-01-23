672e4d3fe6927efce56463cae7a470ba92cf8891
Patrick Bußmann <patrick.bussmann@christmann.info>
2013-01-23 12:08:21 +0100

    Added xml array generator to power supply entity

--- src/Debb/ManagementBundle/Entity/PowerSupply.php
+++ src/Debb/ManagementBundle/Entity/PowerSupply.php
@@ -13,95 +13,117 @@ use Doctrine\ORM\Mapping as ORM;
 class PowerSupply extends Base
 {
 
+	/**
+	 * @var float
+	 *
+	 * @ORM\Column(name="MaxPower", type="float")
+	 */
+	private $maxPower;
 
-    /**
-     * @var float
-     *
-     * @ORM\Column(name="MaxPower", type="float")
-     */
-    private $maxPower;
+	/**
+	 * @var float
+	 *
+	 * @ORM\Column(name="TotalOutputPower", type="decimal")
+	 */
+	private $totalOutputPower;
 
-    /**
-     * @var float
-     *
-     * @ORM\Column(name="TotalOutputPower", type="decimal")
-     */
-    private $totalOutputPower;
+	/**
+	 * @var integer
+	 *
+	 * @ORM\Column(name="Efficiency", type="integer")
+	 */
+	private $efficiency;
 
-    /**
-     * @var integer
-     *
-     * @ORM\Column(name="Efficiency", type="integer")
-     */
-    private $efficiency;
+	/**
+	 * Set maxPower
+	 *
+	 * @param float $maxPower
+	 * @return PowerSupply
+	 */
+	public function setMaxPower($maxPower)
+	{
+		$this->maxPower = $maxPower;
 
+		return $this;
+	}
 
-    /**
-     * Set maxPower
-     *
-     * @param float $maxPower
-     * @return PowerSupply
-     */
-    public function setMaxPower($maxPower)
-    {
-        $this->maxPower = $maxPower;
-    
-        return $this;
-    }
+	/**
+	 * Get maxPower
+	 *
+	 * @return float 
+	 */
+	public function getMaxPower()
+	{
+		return $this->maxPower;
+	}
 
-    /**
-     * Get maxPower
-     *
-     * @return float 
-     */
-    public function getMaxPower()
-    {
-        return $this->maxPower;
-    }
+	/**
+	 * Set totalOutputPower
+	 *
+	 * @param float $totalOutputPower
+	 * @return PowerSupply
+	 */
+	public function setTotalOutputPower($totalOutputPower)
+	{
+		$this->totalOutputPower = $totalOutputPower;
 
-    /**
-     * Set totalOutputPower
-     *
-     * @param float $totalOutputPower
-     * @return PowerSupply
-     */
-    public function setTotalOutputPower($totalOutputPower)
-    {
-        $this->totalOutputPower = $totalOutputPower;
-    
-        return $this;
-    }
+		return $this;
+	}
 
-    /**
-     * Get totalOutputPower
-     *
-     * @return float 
-     */
-    public function getTotalOutputPower()
-    {
-        return $this->totalOutputPower;
-    }
+	/**
+	 * Get totalOutputPower
+	 *
+	 * @return float 
+	 */
+	public function getTotalOutputPower()
+	{
+		return $this->totalOutputPower;
+	}
 
-    /**
-     * Set efficiency
-     *
-     * @param integer $efficiency
-     * @return PowerSupply
-     */
-    public function setEfficiency($efficiency)
-    {
-        $this->efficiency = $efficiency;
-    
-        return $this;
-    }
+	/**
+	 * Set efficiency
+	 *
+	 * @param integer $efficiency
+	 * @return PowerSupply
+	 */
+	public function setEfficiency($efficiency)
+	{
+		$this->efficiency = $efficiency;
 
-    /**
-     * Get efficiency
-     *
-     * @return integer 
-     */
-    public function getEfficiency()
-    {
-        return $this->efficiency;
-    }
+		return $this;
+	}
+
+	/**
+	 * Get efficiency
+	 *
+	 * @return integer 
+	 */
+	public function getEfficiency()
+	{
+		return $this->efficiency;
+	}
+
+	/**
+	 * Returns a array for later converting
+	 * 
+	 * @return array the array for later converting
+	 */
+	public function getXmlArray()
+	{
+		$array = parent::getXmlArray();
+		if ($this->getMaxPower() != null)
+		{
+			$array['MaxPower'] = $this->getMaxPower();
+		}
+		if ($this->getTotalOutputPower() != null)
+		{
+			$array['TotalOutputPower'] = $this->getTotalOutputPower();
+		}
+		if ($this->getEfficiency() != null)
+		{
+			$array['Efficiency'] = $this->getEfficiency();
+		}
+		return $array;
+	}
+
 }
