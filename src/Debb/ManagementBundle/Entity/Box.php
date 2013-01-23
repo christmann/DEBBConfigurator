ace858736a6f9be0efb42831ef75e55f219eb458
Patrick Bußmann <patrick.bussmann@christmann.info>
2013-01-23 12:26:39 +0100

    Added xml array generator to box entity

--- src/Debb/ManagementBundle/Entity/Box.php
+++ src/Debb/ManagementBundle/Entity/Box.php
@@ -13,12 +13,12 @@ use Doctrine\ORM\Mapping as ORM;
 class Box extends \Debb\ConfigBundle\Entity\Dimensions
 {
 
-    /**
-     * @var Image
-     *
+	/**
+	 * @var Image
+	 *
 	 * @ORM\ManyToOne(targetEntity="Debb\ManagementBundle\Entity\File", cascade={"all"})
-     */
-    private $image;
+	 */
+	private $image;
 
 	/**
 	 * @var integer
@@ -26,5 +26,66 @@ class Box extends \Debb\ConfigBundle\Entity\Dimensions
 	 * @ORM\Column(name="slots", type="integer")
 	 */
 	private $slots;
+
+	/**
+	 * Set slots
+	 *
+	 * @param integer $slots
+	 * @return Box
+	 */
+	public function setSlots($slots)
+	{
+		$this->slots = $slots;
+
+		return $this;
+	}
+
+	/**
+	 * Get slots
+	 *
+	 * @return integer 
+	 */
+	public function getSlots()
+	{
+		return $this->slots;
+	}
+
+	/**
+	 * Set image
+	 *
+	 * @param \Debb\ManagementBundle\Entity\File $image
+	 * @return Box
+	 */
+	public function setImage(\Debb\ManagementBundle\Entity\File $image = null)
+	{
+		$this->image = $image;
+
+		return $this;
+	}
+
+	/**
+	 * Get image
+	 *
+	 * @return \Debb\ManagementBundle\Entity\File 
+	 */
+	public function getImage()
+	{
+		return $this->image;
+	}
+
+	/**
+	 * Returns a array for later converting
+	 * 
+	 * @return array the array for later converting
+	 */
+	public function getXmlArray()
+	{
+		$array = parent::getXmlArray();
+		if ($this->getSlots() != null)
+		{
+			$array['Slots'] = $this->getSlots();
+		}
+		return $array;
+	}
 
 }
