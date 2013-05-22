cb6bacb8b9b9dc43e07790dcaa0fb166dd6fc3cf
Patrick Bußmann <patrick.bussmann@christmann.info>
2013-05-22 16:32:18 +0200

    Added grid to room designer - Fixed #1604

--- src/Debb/ConfigBundle/Resources/public/js/room.js
+++ src/Debb/ConfigBundle/Resources/public/js/room.js
@@ -5,6 +5,7 @@ var rackDragOpt = {
 	snapMode: 'outer',
 	stack: '.rackG',
 	revert: 'invalid',
+	grid: [ 20, 20 ],
 	stop: function(event, ui) {
 		$(this).draggable('option', 'revert', 'invalid');
 		var id = getExactId(ui.helper.find('.rackGform').find('div[id!=""]').attr('id'));
