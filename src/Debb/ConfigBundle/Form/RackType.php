1596370e09f198b951c37286b9c2c0d9a06ea3d5
Patrick Bußmann <patrick.bussmann@christmann.info>
2013-06-03 12:21:50 +0200

    Changed form style of racks

--- src/Debb/ConfigBundle/Form/RackType.php
+++ src/Debb/ConfigBundle/Form/RackType.php
@@ -20,16 +20,16 @@ class RackType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('manufacturer', null, array('required' => false))
-            ->add('product', null, array('required' => false))
-            ->add('model', null, array('required' => false))
-            ->add('sizeX', null, array('required' => false))
-            ->add('sizeY', null, array('required' => false))
-            ->add('sizeZ', null, array('required' => false))
-            ->add('spaceTop', null, array('required' => false))
-            ->add('spaceLeft', null, array('required' => false))
-            ->add('spaceBottom', null, array('required' => false))
-            ->add('spaceRight', null, array('required' => false))
+            ->add('manufacturer')
+            ->add('product', null, array('attr' => array('class' => 'noBreakAfterThis')))
+            ->add('model')
+            ->add('sizeX', null, array('required' => false, 'attr' => array('class' => 'noBreakAfterThis'), 'label' => 'SizeX'))
+            ->add('sizeY', null, array('required' => false, 'label' => 'SizeY'))
+            ->add('sizeZ', null, array('required' => false, 'label' => 'SizeZ'))
+            ->add('spaceTop', null, array('required' => false, 'attr' => array('class' => 'noBreakAfterThis'), 'label' => 'SpaceTop'))
+            ->add('spaceBottom', null, array('required' => false, 'label' => 'SpaceBottom'))
+            ->add('spaceLeft', null, array('required' => false, 'attr' => array('class' => 'noBreakAfterThis'), 'label' => 'SpaceLeft'))
+            ->add('spaceRight', null, array('required' => false, 'label' => 'SpaceRight'))
             ->add('nodegroups', 'collection', array(
 				'type' => new \Debb\ManagementBundle\Form\NodegroupToRackType(),
 				'allow_add' => true,
@@ -37,7 +37,7 @@ class RackType extends AbstractType
 				'by_reference' => false,
 				'required' => false,
 			))
-			->add('nodeGroupSize', 'choice', array('choices' => $this->getNodeGroupSizeChoices(), 'required' => false,
+			->add('nodeGroupSize', 'choice', array('choices' => $this->getNodeGroupSizeChoices(), 'required' => false, 'label' => 'RackSize',
 				'empty_value' => false, 'attr' => array('class' => 'updateRackSize')))
         ;
     }
