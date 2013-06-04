3e78c5e5332de073d974880eb20b34b611f94aec
Patrick Bußmann <patrick.bussmann@christmann.info>
2013-06-04 10:01:44 +0200

    Modified chassis

--- src/Debb/ManagementBundle/Form/NodeGroupDraftType.php
+++ src/Debb/ManagementBundle/Form/NodeGroupDraftType.php
@@ -5,6 +5,7 @@ namespace Debb\ManagementBundle\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use Debb\ManagementBundle\Entity\NodeGroupDraft;
 
 /**
  * Class NodeGroupDraftType
@@ -19,21 +20,22 @@ class NodeGroupDraftType extends AbstractType
 	 */
 	public function buildForm(FormBuilderInterface $builder, array $options)
     {
+        $isNew = !array_key_exists('data', $options) || !($options['data'] instanceof NodeGroupDraft) || $options['data']->getId() == null;
         $builder
             ->add('manufacturer')
             ->add('product', null, array('attr' => array('class' => 'noBreakAfterThis')))
             ->add('model')
-			->add('slotsX', 'choice', array('choices' => $this->getSlotsAmount(), 'empty_value' => false, 'attr' => array('style' => 'width: 223px;', 'class' => 'noBreakAfterThis')))
-			->add('slotsY', 'choice', array('choices' => $this->getSlotsAmount(), 'empty_value' => false, 'attr' => array('style' => 'width: 223px;')))
-            ->add('heSize', 'choice', array('choices' => $this->getSlotsAmount(), 'empty_value' => false, 'attr' => array('style' => 'width: 223px;', 'class' => 'noBreakAfterThis')))
-			->add('frontView', 'choice', array('choices' => array(0 => 'Top', 1 => 'Front'), 'empty_value' => false, 'attr' => array('style' => 'width: 223px;'), 'label' => 'View'))
+			->add('slotsX', 'choice', array('choices' => $this->getSlotsAmount(), 'empty_value' => false, 'attr' => array('style' => 'width: 223px;', 'class' => 'noBreakAfterThis'), 'disabled' => !$isNew))
+			->add('slotsY', 'choice', array('choices' => $this->getSlotsAmount(), 'empty_value' => false, 'attr' => array('style' => 'width: 223px;'), 'disabled' => !$isNew))
+            ->add('heSize', 'choice', array('choices' => $this->getSlotsAmount(), 'empty_value' => false, 'attr' => array('style' => 'width: 223px;', 'class' => 'noBreakAfterThis'), 'disabled' => !$isNew))
+			->add('frontView', 'choice', array('choices' => array(0 => 'Top', 1 => 'Front'), 'empty_value' => false, 'attr' => array('style' => 'width: 223px;'), 'label' => 'View', 'disabled' => !$isNew))
             ->add('image', 'plupload')
-            ->add('spaceLeft', 'hidden', array('required' => false))
-            ->add('spaceTop', 'hidden', array('required' => false))
-            ->add('spaceRight', 'hidden', array('required' => false))
-            ->add('spaceBottom', 'hidden', array('required' => false))
-            ->add('spaceFront', 'hidden', array('required' => false))
-            ->add('spaceBehind', 'hidden', array('required' => false))
+            ->add('spaceLeft', 'hidden', array('required' => false, 'disabled' => !$isNew))
+            ->add('spaceTop', 'hidden', array('required' => false, 'disabled' => !$isNew))
+            ->add('spaceRight', 'hidden', array('required' => false, 'disabled' => !$isNew))
+            ->add('spaceBottom', 'hidden', array('required' => false, 'disabled' => !$isNew))
+            ->add('spaceFront', 'hidden', array('required' => false, 'disabled' => !$isNew))
+            ->add('spaceBehind', 'hidden', array('required' => false, 'disabled' => !$isNew))
         ;
     }
 
