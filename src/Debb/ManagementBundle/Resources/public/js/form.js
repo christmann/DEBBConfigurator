fd694472fbc6dc1da09b27d9a0c57f4d6b1baf92
Fabian Martin <fabian.martin@christmann.info>
2013-06-14 16:40:04 +0200

    show message, if form has uncommitted changes, closes #1660

--- src/Debb/ManagementBundle/Resources/public/js/form.js
+++ src/Debb/ManagementBundle/Resources/public/js/form.js
@@ -1,4 +1,57 @@
-$(function()
+function s4()
 {
-	$('.noBreakAfterThis').parent().css('float', 'left').css('margin-right', '30px');
+    return Math.floor((1 + Math.random()) * 0x10000)
+        .toString(16)
+        .substring(1);
+}
+
+function guid()
+{
+    return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4();
+}
+
+$(function ()
+{
+    $('.noBreakAfterThis').parent().css('float', 'left').css('margin-right', '30px');
+
+    var changed = false;
+    var isSubmitted = false;
+    var inputCache = {};
+    var fieldName = 'data-input-check-id';
+    var selector = 'input,select';
+
+    // map fields
+    $(selector).each(function ()
+    {
+        var uid = guid();
+        $(this).attr(fieldName, uid);
+        inputCache[uid] = $(this).val();
+    });
+
+    // funny function, store all input values into an associative array
+    var checkField = function ()
+    {
+        var uid = $(this).attr(fieldName);
+        if (uid && typeof inputCache[uid] != "undefined") {
+            if (inputCache[uid] != $(this).val()) {
+                changed = true;
+            }
+        }
+        else {
+            changed = true;
+        }
+    }
+
+    $('form').bind('submit', function ()
+    {
+        isSubmitted = true;
+    });
+    $(window).bind('beforeunload', function ()
+    {
+        changed = false;
+        $(selector).each(checkField);
+        if (!isSubmitted && changed) {
+            return 'The change are NOT yet saved. Click OK to continue or CANCEL to stay on the site.';
+        }
+    });
 });
