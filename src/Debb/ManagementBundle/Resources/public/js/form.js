448c84f5e0c697dd3938c63c94b6a32a3e92f919
Patrick Bußmann <patrick.bussmann@christmann.info>
2013-09-02 16:39:40 +0200

    Fixed bug with forms

--- src/Debb/ManagementBundle/Resources/public/js/form.js
+++ src/Debb/ManagementBundle/Resources/public/js/form.js
@@ -14,33 +14,17 @@ $(function ()
 {
     $('.noBreakAfterThis').parent().css('float', 'left').css('margin-right', '30px');
 
-    var changed = false;
-    var isSubmitted = false;
-    var inputCache = {};
-    var fieldName = 'data-input-check-id';
-    var selector = 'input,select';
+    var changed = false,
+        isSubmitted = false,
+        selector = 'input,select';
 
-    // map fields
-    $(selector).each(function ()
+    $(document).on('change', selector, function(e)
     {
-        var uid = guid();
-        $(this).attr(fieldName, uid);
-        inputCache[uid] = $(this).val();
-    });
-
-    // funny function, store all input values into an associative array
-    var checkField = function ()
-    {
-        var uid = $(this).attr(fieldName);
-        if (uid && typeof inputCache[uid] != "undefined") {
-            if (inputCache[uid] != $(this).val()) {
-                changed = true;
-            }
-        }
-        else {
+        if(e.hasOwnProperty('originalEvent'))
+        {
             changed = true;
         }
-    }
+    });
 
     $('form').bind('submit', function ()
     {
@@ -48,8 +32,6 @@ $(function ()
     });
     $(window).bind('beforeunload', function ()
     {
-        changed = false;
-        $(selector).each(checkField);
         if (!isSubmitted && changed) {
             return 'The change are NOT yet saved. Click OK to continue or CANCEL to stay on the site.';
         }
