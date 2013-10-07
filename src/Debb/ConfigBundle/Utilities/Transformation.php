0f94b5933e01e8cf2d1d758af738eb9941ea6a30
Patrick Bußmann <patrick.bussmann@christmann.info>
2013-10-07 10:01:37 +0200

    Added negation to chassis flow pumps

--- src/Debb/ConfigBundle/Utilities/Transformation.php
+++ src/Debb/ConfigBundle/Utilities/Transformation.php
@@ -51,8 +51,8 @@ class Transformation
 		if ($className == 'Rack' || $className == 'FlowPump')
 		{
 			/** @var $connector RackToRoom */
-			$posX = $connector->getPosX() * ($className == 'FlowPump' && XMLController::get_real_class($connector) == 'FlowPumpToChassis' ? 1 : 10);
-			$posY = $connector->getPosY() * ($className == 'FlowPump' && XMLController::get_real_class($connector) == 'FlowPumpToChassis' ? 1 : 10);
+			$posX = $connector->getPosX() * ($className == 'FlowPump' && XMLController::get_real_class($connector) == 'FlowPumpToChassis' ? -1 : 10);
+			$posY = $connector->getPosY() * ($className == 'FlowPump' && XMLController::get_real_class($connector) == 'FlowPumpToChassis' ? -1 : 10);
 			$posZ = $connector->getPosZ();
 			$rotation = $connector->getRotation();
 			/** @var $children Rack */
