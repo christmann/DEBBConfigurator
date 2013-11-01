65c12de80b918b8501e24e7ca4e75facc813c0c9
Patrick Bußmann <patrick.bussmann@christmann.info>
2013-11-01 16:14:37 +0100

    Fixed bug with wrong position

--- src/Debb/ConfigBundle/Utilities/Transformation.php
+++ src/Debb/ConfigBundle/Utilities/Transformation.php
@@ -70,9 +70,9 @@ class Transformation
 		else if ($className == 'FlowPump')
 		{
 			/** @var $connector FlowPumpToChassis */
-			$posX = $connector->getCustomPosX() ? $connector->getCustomPosX() * 1000 : ($connector->getPosX() * ($className == 'FlowPump' && XMLController::get_real_class($connector) == 'FlowPumpToChassis' ? -1 : 10));
-			$posY = $connector->getCustomPosY() ? $connector->getCustomPosY() * 1000 : ($connector->getPosY() * ($className == 'FlowPump' && XMLController::get_real_class($connector) == 'FlowPumpToChassis' ? -1 : 10));
-			$posZ = $connector->getCustomPosZ() ? $connector->getCustomPosZ() * 1000 : $connector->getPosZ();
+			$posX = $connector->getCustomPosX() ? $connector->getCustomPosX() * (XMLController::get_real_class($connector) == 'FlowPumpToChassis' ? -1000 : 1000) : ($connector->getPosX() * (XMLController::get_real_class($connector) == 'FlowPumpToChassis' ? -1 : 10));
+			$posY = $connector->getCustomPosY() ? $connector->getCustomPosY() * (XMLController::get_real_class($connector) == 'FlowPumpToChassis' ? -1000 : 1000) : ($connector->getPosY() * (XMLController::get_real_class($connector) == 'FlowPumpToChassis' ? -1 : 10));
+			$posZ = $connector->getCustomPosZ() ? $connector->getCustomPosZ() * (XMLController::get_real_class($connector) == 'FlowPumpToChassis' ? -1000 : 1000) : $connector->getPosZ();
 			$rotation = $connector->getRotation();
 			/** @var $children FlowPump */
 			self::$transformations[] = array($posX, $posY, $posZ, $rotation, $children->getSizeX() * 1000, $children->getSizeZ() * 1000, $children->getSizeY() * 1000);
