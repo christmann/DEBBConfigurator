082b0c660b8b1daeeb14dfcd9de672644133ff92
Patrick Bußmann <patrick.bussmann@christmann.info>
2014-01-31 09:08:27 +0100

    Added license to each file

--- src/Debb/ConfigBundle/DependencyInjection/DebbConfigExtension.php
+++ src/Debb/ConfigBundle/DependencyInjection/DebbConfigExtension.php
@@ -1,4 +1,23 @@
 <?php
+/**
+ * DEBBConfigurator - A configurator for DEBB component and PLMXML files
+ * Copyright (C) 2013-2014 christmann informationstechnik + medien GmbH & Co. KG
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 51 Franklin St, Fifth Floor,
+ * Boston, MA  02110-1301, USA.
+ */
 
 namespace Debb\ConfigBundle\DependencyInjection;
 
@@ -11,6 +30,8 @@ use Symfony\Component\DependencyInjection\Loader;
  * This is the class that loads and manages your bundle configuration
  *
  * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html}
+ *
+ * @author Patrick Bußmann <patrick.bussmann@christmann.info>
  */
 class DebbConfigExtension extends Extension
 {
