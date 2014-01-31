7a0cca3beb11cef8d6fba4c13efbbc4b38fd4aeb
Patrick Bußmann <patrick.bussmann@christmann.info>
2014-01-31 09:08:27 +0100

    Added license to each file

--- src/Debb/ManagementBundle/Form/PStateLoadPowerUsageType.php
+++ src/Debb/ManagementBundle/Form/PStateLoadPowerUsageType.php
@@ -1,4 +1,23 @@
 <?php
+/**
+ * DEBBConfigurator - A configurator for DEBB component and PLMXML files
+ * Copyright (C) 2013-2014 christmann informationstechnik + medien GmbH & Co. KG
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 51 Franklin St, Fifth Floor,
+ * Boston, MA  02110-1301, USA.
+ */
 
 namespace Debb\ManagementBundle\Form;
 
@@ -6,9 +25,14 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 
+/**
+ * Class PStateLoadPowerUsageType
+ * @package Debb\ManagementBundle\Form
+ * @author Patrick Bußmann <patrick.bussmann@christmann.info>
+ */
 class PStateLoadPowerUsageType extends AbstractType
 {
-        /**
+    /**
      * @param FormBuilderInterface $builder
      * @param array $options
      */
